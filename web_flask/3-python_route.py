b6328423fdd70e945076baecb93c2cb0b493701e
root <Olaleyebode001@gmail.com>
2023-02-10 17:50:11 -0800

    Python is cool

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -20,5 +20,18 @@ def hbnb():
     return ("HBNB")
 
 
+@app.route("/c/<text>", strict_slashes=False)
+def cText(text):
+    """display C followed by the value of the text variable"""
+    return "C {}".format(text.replace("_", " "))
+
+
+@app.route('/python', strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def pythonText(text="is cool"):
+    """display Python followed by the value of the text variable"""
+    return "Python {}".format(text.replace("_", " "))
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=None)
